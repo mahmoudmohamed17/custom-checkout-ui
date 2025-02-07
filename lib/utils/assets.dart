6bb771badc3ac54e32b87abfe44c50e415081915
mahmoudmohamed17 <ma5975986@gmail.com>
2025-02-07 21:35:16 +0200

    setup assets and styles

---
+++ lib/utils/assets.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for applePay
+  /// assets/apple-pay.png
+  static const String applePay = "assets/apple-pay.png";
+
+  /// Assets for arrowBack
+  /// assets/arrow-back.svg
+  static const String arrowBack = "assets/arrow-back.svg";
+
+  /// Assets for basket
+  /// assets/basket.png
+  static const String basket = "assets/basket.png";
+
+  /// Assets for card
+  /// assets/card.svg
+  static const String card = "assets/card.svg";
+
+  /// Assets for paypal
+  /// assets/paypal.svg
+  static const String paypal = "assets/paypal.svg";
+}
+
---
+++ lib/utils/styles.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const TextStyle regular18 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle semiBold18 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle medium9 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 9,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle semiBold9 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 9,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle regular4 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 4,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle semiBold24 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle medium12 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle semiBold12 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle sfProSemiBold17 = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontSize: 17,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle medium25 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 25,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle regular5 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 5,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle medium22 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 22,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle regular20 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle semiBold20 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+  );
+}
