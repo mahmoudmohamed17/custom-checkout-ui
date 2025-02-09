2f585763692b6509544c179fed8978ce6883a08b
mahmoudmohamed17 <ma5975986@gmail.com>
2025-02-09 20:51:32 +0200

    create api service

---
+++ lib/services/stripe_service.dart
@@ -0,0 +1,8 @@
+import 'package:custom_checkout_ui/models/payment_intent_input_model.dart';
+import 'package:custom_checkout_ui/models/payment_intent_model/payment_intent_model.dart';
+
+class StripeService {
+  PaymentIntentModel createPaymentIntent(PaymentIntentInputModel model) {
+    
+  }
+}
