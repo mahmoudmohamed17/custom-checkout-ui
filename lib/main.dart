c17375dc9c89ab2d8023f37e26b0ed632b890e43
mahmoudmohamed17 <ma5975986@gmail.com>
2025-02-10 21:12:08 +0200

    some fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,13 @@
 import 'package:custom_checkout_ui/managers/payment_cubit/payment_cubit.dart';
 import 'package:custom_checkout_ui/utils/api_keys.dart';
 import 'package:custom_checkout_ui/views/my_cart_view.dart';
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 
 void main() {
   Stripe.publishableKey = ApiKeys.publishableKey;
-  runApp(DevicePreview(
-    builder: (context) => const MyApp(),
-  ));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -27,3 +24,5 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// card number test: 4242424242424242
--- lib/models/payment_intent_input_model.dart
+++ lib/models/payment_intent_input_model.dart
@@ -6,7 +6,7 @@ class PaymentIntentInputModel {
   // convert the data into json formate indeed to create the payment intent model
   // using the post request from the stripe api
   Map<String, dynamic> toJson() => {
-        "amount": amount,
+        "amount": '${amount}00',
         "currency": currency,
       };
 }
